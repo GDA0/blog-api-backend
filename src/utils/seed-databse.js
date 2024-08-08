250d24f4b985215705e7fa1a670fd27c1be49645
GDA0 <gideondelaliadeti0@gmail.com>
2024-08-08 20:48:46 +0000

    Update script for seeding the database and reseed the db

--- src/utils/seed-databse.js
+++ src/utils/seed-databse.js
@@ -1,34 +1,72 @@
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
 const { faker } = require("@faker-js/faker");
 
 const prisma = new PrismaClient();
-const NUMBER_OF_POSTS = 25;
+
+const AUTHOR_ID = "3519e331-3f78-4e80-8b79-22e5b6c3dd5e";
 
 async function main() {
-  // Create some posts for the admin user
-  for (let i = 0; i < NUMBER_OF_POSTS; i++) {
-    await prisma.post.create({
+  console.log("Seeding users...");
+  // Create users
+  const users = [];
+  for (let i = 0; i < 100; i++) {
+    const hashedPassword = await bcrypt.hash("password", 10);
+    const user = await prisma.user.create({
       data: {
-        id: faker.string.uuid(),
-        title: faker.lorem.sentence(),
-        content: faker.lorem.paragraphs({ min: 5, max: 7 }),
-        authorId: "3519e331-3f78-4e80-8b79-22e5b6c3dd5e",
-        published: faker.datatype.boolean(),
-        createdAt: faker.date.past(),
-        updatedAt: faker.date.recent(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        username: faker.internet.userName(),
+        password: hashedPassword,
       },
     });
+    users.push(user);
   }
+  console.log("Users seeded successfully.");
 
-  console.log("Seeding completed");
+  console.log("Seeding posts and comments...");
+  // Create posts
+  for (let i = 0; i < 50; i++) {
+    const published = faker.datatype.boolean();
+    const post = await prisma.post.create({
+      data: {
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraphs({ min: 3, max: 5 }),
+        authorId: AUTHOR_ID,
+        published: published,
+        updatedAt: faker.datatype.boolean()
+          ? faker.date.recent()
+          : faker.date.past(),
+      },
+    });
+
+    // Create comments for the post only if published
+    if (published) {
+      const numberOfComments = faker.number.int({ min: 10, max: 30 });
+      for (let j = 0; j < numberOfComments; j++) {
+        const randomUser =
+          users[faker.number.int({ min: 0, max: users.length - 1 })];
+        await prisma.comment.create({
+          data: {
+            content: faker.lorem.sentences(2),
+            postId: post.id,
+            authorId: randomUser.id,
+            updatedAt: faker.datatype.boolean()
+              ? faker.date.recent()
+              : faker.date.past(),
+          },
+        });
+      }
+    }
+  }
+  console.log("Posts and comments seeded successfully.");
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
+  .catch((e) => {
     console.error(e);
-    await prisma.$disconnect();
     process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
   });
